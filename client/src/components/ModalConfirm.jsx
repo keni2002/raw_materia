3bbd0e3e03467358273cd0fb97863c6d190bcff0
keni2002 <yerbisuniverses@gmail.com>
2024-02-29 03:45:13 -0500

    todo bien

--- client/src/components/ModalConfirm.jsx
+++ client/src/components/ModalConfirm.jsx
@@ -3,6 +3,7 @@ import { useDeleteComercialMutation } from "../services/apiTable";
 import toast from "react-hot-toast";
 import { setIsOpenModalConfirm } from "../features/booleanos";
 import { useEffect, useState } from "react";
+import { unwrapResult } from "@reduxjs/toolkit";
 
 export default function ModalConfirm() {
     const { iD: id } = useSelector(state => state.booleanos);
@@ -22,10 +23,16 @@ export default function ModalConfirm() {
     }, [text])
 
     const deleteHandler = async () => {
-        await deleteComercial(id);
-        isSuccess ? toast.success('Se ha eliminado el comercial') : toast.error('No se ha podido eliminar el comercial');
-        dispatch(setIsOpenModalConfirm(false));
-        setText('')
+        try {
+            const result = await deleteComercial(id);
+            unwrapResult(result);
+            toast.success('Se ha eliminado el comercial');
+            dispatch(setIsOpenModalConfirm(false));
+            setText('')
+        }catch(err){
+            toast.error('Error al eliminar')
+        }
+        
         
     }
     const handleText = (e) => {
@@ -46,10 +53,10 @@ export default function ModalConfirm() {
                             <label htmlFor="confirm">Escriba: voy a Eliminar un usuario</label>
                             <input type="text" onChange={handleText} value={text} id="confirm" name="confirm" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-gray-500 focus:border-gray-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500 dark:shadow-sm-light" required />
                         </div>
-                        {disabled && <button  onClick={deleteHandler} type="button" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
+                        {!disabled && <button onClick={deleteHandler} type="button" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
                             Sí, estoy seguro
                         </button>}
-                        <button  onClick={() => dispatch(setIsOpenModalConfirm(false))} type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">No, cancelar</button>
+                        <button onClick={() => dispatch(setIsOpenModalConfirm(false))} type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">No, cancelar</button>
                     </div>
                 </div>
             </div>
